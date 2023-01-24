7065d2661b979c48f1437e9ae6cd4bd746aeb486
merzainc <believe@noclon.co.zw>
2023-01-24 09:53:12 +0200

    Provided destroy action loader

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css';
 import EditContact, { action as editAction } from "./routes/edit";
 import { Root, loader as rootLoader, action as rootAction } from './routes/root';
 import Contact, { loader as contactLoader } from './routes/contact';
+import { action as destroyAction } from "./routes/destroy";
 import ErrorPage from './error-page';
 
 const router = createBrowserRouter([
@@ -21,6 +22,11 @@ const router = createBrowserRouter([
         loader: contactLoader,
         action: editAction
       },
+      {
+        path: "contacts/:contactId/destroy",
+        action: destroyAction,
+        errorElement: < div > Oops! There was an error.</div >
+      }
     ]
   }
 ]);
