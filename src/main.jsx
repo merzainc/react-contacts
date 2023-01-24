9a152516aceded5e80bbbb492dbd735db4b6c141
merzainc <believe@noclon.co.zw>
2023-01-24 09:48:30 +0200

    Refactored the routes for the app

---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import './index.css';
+import EditContact, { action as editAction } from "./routes/edit";
+import { Root, loader as rootLoader, action as rootAction } from './routes/root';
+import Contact, { loader as contactLoader } from './routes/contact';
+import ErrorPage from './error-page';
+
+const router = createBrowserRouter([
+  {
+    path: '/', element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      { path: '/contacts/:contactId', element: <Contact />, loader: contactLoader },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction
+      },
+    ]
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
